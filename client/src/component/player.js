8e553fa6e8e278023ebb866cf4f360622bf54907
Aman941 <aman.verma9412345@gmail.com>
2020-04-05 00:38:54 +0530

    player added

--- client/src/component/player.js
+++ client/src/component/player.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 
-const videoIdA = 'XxVg_s8xAms';
-const videoIdB = '-DX3vJiqxm4';
+const videoIdA = '-DX3vJiqxm4';
 
 export default class Player extends React.Component {
   constructor(props) {
@@ -10,20 +9,45 @@ export default class Player extends React.Component {
 
     this.state = {
       videoId: videoIdA,
+      tempId: '',
       player: null,
     };
 
     this.onReady = this.onReady.bind(this);
-    this.onChangeVideo = this.onChangeVideo.bind(this);
     this.onPlayVideo = this.onPlayVideo.bind(this);
     this.onPauseVideo = this.onPauseVideo.bind(this);
+    this.onStateChange = this.onStateChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onReady(event) {
-    console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); 
     this.setState({
       player: event.target,
     });
+    event.target.pauseVideo();
+  }
+
+  onChange(event) {
+    this.setState({tempId:event.target.value});
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    console.log("submit hua");
+    // parsing url
+    var video_id = this.state.tempId;
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = video_id.match(regExp);
+    if (match&&match[7].length === 11){
+        video_id = match[7];
+    }
+    this.setState({videoId:video_id});
+  }
+
+  onStateChange(event)
+  {
+    console.log(event.target.getCurrentTime());
   }
 
   onPlayVideo() {
@@ -34,24 +58,24 @@ export default class Player extends React.Component {
     this.state.player.pauseVideo();
   }
 
-  onChangeVideo() {
-    this.setState((state) => ({
-      videoId: state.videoId === videoIdA ? videoIdB : videoIdA,
-    }));
-  }
 
   render() {
     return (
-      <div>
-        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
+      <div align='center'>
+        <form>
+          <input type = 'text' placeholder = 'Enter Url' onChange = {this.onChange}/>
+          <button type = 'submit' onClick = {this.onSubmit}>Submit</button>
+        </form>
+        <YouTube 
+        videoId={this.state.videoId} 
+        onReady={this.onReady}
+        onStateChange={this.onStateChange}
+         />
         <button type="button" onClick={this.onPlayVideo}>
           Play
         </button>
         <button type="button" onClick={this.onPauseVideo}>
           Pause
-        </button>
-        <button type="button" onClick={this.onChangeVideo}>
-          Change Video
         </button>
       </div>
     );
