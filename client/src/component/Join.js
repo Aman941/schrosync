3be29ba9a467b8ac47f31c872a7cdd3ece3fe3b2
Aman941 <aman.verma9412345@gmail.com>
2020-04-07 15:52:52 +0530

    last commit

--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -1,26 +1,27 @@
-import React , {useState } from 'react';
-import { Link } from 'react-router-dom';
 
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
-export default function Join(){
-    const [Name , setName] = useState('');
-    const [Room , setRoom] = useState('');
+import './style/Join.css';
 
-    return(
-    <div className = 'outerContainer'>
-        <div className = 'InnerContainer'>
-            <h1>Name</h1>
-            <input className = 'NameInput' type = 'text' placeholder = 'Name' onChange = {(e) => setName(e.target.value)} />
-            <h1>Room</h1>
-            <input className = 'RoomInput' type = 'text' placeholder = 'Room' onChange = {(e) => setRoom(e.target.value)} />
-            <Link onClick={(e) => (!Name || !Room) ? e.preventDefault() : null} to={`/chat?name=${Name}&room=${Room}`}>
-                <button className={'button mt-20'} type="submit">Sign In</button>
-            </Link>
+export default function SignIn() {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Schro-Sync</h1>
+        <div>
+          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
+        <div>
+          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
+        </div>
+        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className={'button mt-20'} type="submit">Sign In</button>
+        </Link>
+      </div>
     </div>
-    );
+  );
 }
-
-
-
-
